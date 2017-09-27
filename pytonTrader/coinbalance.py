1e001b688630ebccd3e89ecbd213bba81e0b7a48
Codlydodly <zach.yewman@gmail.com>
2017-09-27 15:40:10 +0100

    tidied balance print

--- pytonTrader/coinbalance.py
+++ pytonTrader/coinbalance.py
@@ -7,30 +7,24 @@ class mo_money_mo_problems():
 	 def __init__(self):
 	 	#setup any variables and call the classes
 		public_key = raw_input("Enter wallet public key: ")	 	
-		#self.pull_wallet_data(public_key)
 		wallet_data = self.pull_wallet_data(public_key)
-		print(wallet_data)		
+		self.print_current_wallet_info(wallet_data)		
 
 
 	 def pull_wallet_data(self, wallet_address):
 	 	#pull the data from the api
-		etherscan = urllib.urlopen("https://api.etherscan.io/api?module=account&action=balance&address="+wallet_address+"&tag=latest&apikey=YourApiKeyToken").read()
-		
+		etherscan = urllib.urlopen("https://api.etherscan.io/api?module=account&action=balance&address="+wallet_address+"&tag=latest&apikey=YourApiKeyToken").read()		
 		etherscan = json.loads(etherscan)		
 		wallet = etherscan["result"]
-
 		return wallet 	
 
 
-	 def get_current_coin_price_in_usd(self, coin):
-	 	return current_coin_price
+	 def print_current_wallet_info(self, wallet):
+	 	print("Ethereum balance: "+wallet)
 
+         def get_current_coin_price_in_usd(self, coin):
+                return current_coin_price
 
-
-	 def print_current_wallet_info(self):
-	 	print("Starting_fiat: "+starting_fiat)
-	 	print("Current fiat: "+current_fiat)
-	 	print("Current ethereum: "+current_eth)
 
 
 mmmp = mo_money_mo_problems()
