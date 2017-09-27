6aceb47ae86d4b7ef4d166ca9083c28fdf04b6f0
Codlydodly <zach.yewman@gmail.com>
2017-09-27 17:16:46 +0100

    gets current eth price + calculates usd value of eth in wallet

--- pytonTrader/coinbalance.py
+++ pytonTrader/coinbalance.py
@@ -6,16 +6,23 @@ class mo_money_mo_problems():
 
 	 def __init__(self):
 	 	#setup any variables and call the classes
+		eth_price = self.get_current_coin_price_in_usd("eth")
 		public_key = raw_input("Enter wallet public key: ")
 		wallet_data = self.pull_wallet_data(public_key)
+		if (wallet_data == False): 
+			print("no thank you")
 		wallet_data = self.convert_wei_to_eth(wallet_data)
-		self.print_current_wallet_info(wallet_data)		
+		usd_value = self.calculate_worth_usd(wallet_data, eth_price)
+		self.print_current_wallet_info(wallet_data,usd_value)
 
 
 	 def pull_wallet_data(self, wallet_address):
 	 	#pull the data from the api
 		etherscan = urllib.urlopen("https://api.etherscan.io/api?module=account&action=balance&address="+wallet_address+"&tag=latest&apikey=YourApiKeyToken").read()		
-		etherscan = json.loads(etherscan)		
+		etherscan = json.loads(etherscan)
+		status = etherscan["status"]
+		if (status == 0): 
+			return False 
 		wallet = etherscan["result"]
 		return wallet
 
@@ -26,11 +33,21 @@ class mo_money_mo_problems():
 			eth = 0
 		return eth
 
-	 def print_current_wallet_info(self, wallet):
-	 	print("Ethereum balance: "+str(wallet))
+	 def print_current_wallet_info(self, eth_balance, usd_balance):
+	 	print("Ethereum balance: "+str(eth_balance)+" worth: $"+str(usd_balance))
+		
+
+	 def calculate_worth_usd(self, total_eth,current_price):
+		price = float(total_eth) * float(current_price)
+		price = "{0:.2f}".format(price)
+		return price
 
          def get_current_coin_price_in_usd(self, coin):
-                return current_coin_price
+                coinscan= urllib.urlopen("https://api.coinmarketcap.com/v1/ticker/?limit=10").read()
+		coinscan = json.loads(coinscan)
+		eth_info = coinscan[1]
+		eth_price = eth_info["price_usd"]		
+		return eth_price
 
 
 
