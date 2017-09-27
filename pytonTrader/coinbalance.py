82c5835485d6b5dfbbec17aed440b5c75b0e19a1
Codlydodly <zach.yewman@gmail.com>
2017-09-27 16:21:39 +0100

    Now returns Eth balance in Eth not wei

--- pytonTrader/coinbalance.py
+++ pytonTrader/coinbalance.py
@@ -6,8 +6,9 @@ class mo_money_mo_problems():
 
 	 def __init__(self):
 	 	#setup any variables and call the classes
-		public_key = raw_input("Enter wallet public key: ")	 	
+		public_key = raw_input("Enter wallet public key: ")
 		wallet_data = self.pull_wallet_data(public_key)
+		wallet_data = self.convert_wei_to_eth(wallet_data)
 		self.print_current_wallet_info(wallet_data)		
 
 
@@ -16,11 +17,17 @@ class mo_money_mo_problems():
 		etherscan = urllib.urlopen("https://api.etherscan.io/api?module=account&action=balance&address="+wallet_address+"&tag=latest&apikey=YourApiKeyToken").read()		
 		etherscan = json.loads(etherscan)		
 		wallet = etherscan["result"]
-		return wallet 	
+		return wallet
 
+	 def convert_wei_to_eth(self, wei):
+		try:
+			eth = float(wei) / float(10**18)
+		except:
+			eth = 0
+		return eth
 
 	 def print_current_wallet_info(self, wallet):
-	 	print("Ethereum balance: "+wallet)
+	 	print("Ethereum balance: "+str(wallet))
 
          def get_current_coin_price_in_usd(self, coin):
                 return current_coin_price
